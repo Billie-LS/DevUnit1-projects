b17bb7c5447329e9a7c74965b8acbbf410cf540d
Billie-LS <lskylizard@yahoo.com>
2024-01-08 11:28:26 -0600

    console.log & \n and \t

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,16 @@
+// testing condole.log() function
 console.log('Hello, JavaScript.');
 console.log(2001);
 console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
 console.log("LaunchCode was founded in", 2013);
+
+// testing the \n function (i.e. new line function)
+console.log('python\nJavaScript\nJava\nC#\nRust');
+
+// testing the \t function (i.e. tab function)
+console.log('python\tJavaScript\tJava\tC#\tRust');
+
+// testing the \n function (i.e. new line function) and the \t function (i.e. tab function)
+console.log('\tpython\n\tJavaScript\n\tJava\n\tC#\n\tRust');
