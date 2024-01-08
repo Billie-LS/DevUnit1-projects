4d51d49462e6dbdf13165387abee9eb393f79d39
Billie-LS <lskylizard@yahoo.com>
2024-01-08 11:48:08 -0600

    exercise02 \n and \t

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,18 @@
+// testing \n and \t functions
 console.log("Some Programming Languages:");
+console.log("Python, JavaScript, Java, C#,Swift");
 
-console.log("Python\nJavaScript\nJava\nC#\nSwift");
+// testing \n
+console.log("\nSome Programming Languages:");
+
+console.log("Python\nJavaScript\nJava\nC#\nSwift\n");
+
+// testing \t
+console.log("\tSome Programming Languages:");
+
+console.log("\tPython\tJavaScript\tJava\tC#\tSwift");
+
+// testing \n and \t functions
+console.log("\n\tSome Programming Languages:");
+
+console.log("\tPython\n\tJavaScript\n\tJava\n\tC#\n\tSwift\n\t");
