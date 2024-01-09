27654f8608b578665cbc5378ee2c6409222ef5d1
Billie-LS <lskylizard@yahoo.com>
2024-01-09 12:47:16 -0600

    readline-sync greeting and \n and \t

--- data-and-variables/chapter-examples/readline-greeting-program.js
+++ data-and-variables/chapter-examples/readline-greeting-program.js
@@ -1,3 +1,9 @@
-const input = require('readline-sync');
+// const input = require('readline-sync');
 
-let name = input.question("Enter your name: ");
+// let name = input.question("Enter your name: ");
+
+const inputQuestion1 = require('readline-sync');
+const inputQuestion2 = require('readline-sync');
+let nameFirst = inputQuestion1.question("Enter your first name: ");
+let nameLast = inputQuestion2.question("Enter your last name: ");
+console.log('First name: ' + nameFirst + '\nLast name: ' + nameLast + '\nLast, First: ' + nameFirst+',', nameLast)
