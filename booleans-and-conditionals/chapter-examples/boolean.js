8d729fe01deaed173dc56d39dedd9a12b35d0e97
Billie-LS <lskylizard@yahoo.com>
2024-01-09 16:36:17 -0600

    boolean exercises

---
+++ booleans-and-conditionals/chapter-examples/boolean.js
@@ -0,0 +1,23 @@
+
+console.log(true);
+console.log(typeof true);
+console.log(typeof false);
+
+console.log(typeof true);
+console.log(typeof "true");
+
+console.log(Boolean("true"));  // true
+console.log(Boolean("TRUE"));  // true
+console.log(Boolean(0));  // false
+console.log(Boolean(1));  // true
+console.log(Boolean(''));  // false
+console.log(Boolean('LaunchCode'));  // true
+
+console.log(5 == 5);  // true
+console.log(5 == 6);  // false
+
+console.log(true == "true");  // false
+
+console.log(7 == "7");  // true
+console.log(0 == false);  // true
+console.log(0 == '');  // true
