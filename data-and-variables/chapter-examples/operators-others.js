788680f0c0348b64c877c2c631a9b2f10e17a91d
Billie-LS <lskylizard@yahoo.com>
2024-01-09 12:45:08 -0600

    operators, other operators, compound operators, etc...

---
+++ data-and-variables/chapter-examples/operators-others.js
@@ -0,0 +1,23 @@
+/*
+OTHER OPERATORS
+The String Operator +
+the + operator can be used with string operands to concatenate, 
+or join together two strings
+i.e. "Launch" + "Code" evaluates to "LaunchCode"
+*/
+console.log(1 + 1);
+console.log(typeof(1 + 1));
+
+console.log("1" + "1");
+console.log(typeof("1" + "1"));
+
+// Compound Assignment Operators
+let x = 1;
+x = x + 1;
+
+console.log(x);
+
+let y = 1;
+y += 1;
+
+console.log(y);
