5cbad4bb3c2fea473b9e9a862e25a232c6010173
Billie-LS <lskylizard@yahoo.com>
2024-01-09 13:15:07 -0600

    rust shuttle to moon mars tutorial

---
+++ data-and-variables/exercises/space_mission/target/debug/deps/space_mission-dcccb8046a774cb9.d
@@ -0,0 +1,5 @@
+/Users/lokiskylizard/Desktop/DevExercises/DevUnit1-projects/data-and-variables/exercises/space_mission/target/debug/deps/space_mission-dcccb8046a774cb9: src/main.rs
+
+/Users/lokiskylizard/Desktop/DevExercises/DevUnit1-projects/data-and-variables/exercises/space_mission/target/debug/deps/space_mission-dcccb8046a774cb9.d: src/main.rs
+
+src/main.rs:
