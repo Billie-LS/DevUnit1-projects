5589dab52ea1ca006a1ca72b64ff68e4897cea3d
Billie-LS <lskylizard@yahoo.com>
2024-01-09 12:52:09 -0600

    typeof

--- data-and-variables/chapter-examples/type-of.js
+++ data-and-variables/chapter-examples/type-of.js
@@ -1,3 +1,7 @@
-console.log(typeof "Hello, World!");
-console.log(typeof 17);
-console.log(typeof 3.14);
+console.log(typeof "Hello, People");
+
+console.log(typeof "17");
+console.log(typeof "3.2");
+
+console.log(typeof 'This is a string');
+console.log(typeof "And so is this");
