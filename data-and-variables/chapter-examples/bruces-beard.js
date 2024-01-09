420e803e868c008c63fb5ae6cec5e9d3417f0332
Billie-LS <lskylizard@yahoo.com>
2024-01-09 12:37:20 -0600

    exercises in console.log quotations mismatch errors

--- data-and-variables/chapter-examples/bruces-beard.js
+++ data-and-variables/chapter-examples/bruces-beard.js
@@ -1,1 +1,4 @@
-console.log('Bruce's beard');
+// this code won't work because of quote types mismatch
+// console.log('Bruce's beard');
+
+console.log("Bruce's beard");  // will work because correct quotes
