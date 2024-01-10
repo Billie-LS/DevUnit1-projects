61dc24f3a7b1ecda1519462d6acc5f8c55f342d6
Billie-LS <lskylizard@yahoo.com>
2024-01-10 16:14:15 -0600

    studio work, template literals and unicode character

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -17,15 +17,63 @@ let preparedForLiftOff = true; // boolean indicating 'go' or 'no-go'
 
 
 // conditional logic to verify astronautCount for shuttle does not exceed 7
+if (astronautCount<= 7); {
 
-// add logic below to verify all astronauts are ready
+}
 
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
+// conditional logic to verify astronautStatus is 'ready'
+if (astronautStatus === 'ready');{
 
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+}
 
-// add logic below to verify the fuel level is at 100%
+// conditional logic to verify fuel temperature is in the required range i.e. between -150 and -300
+if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150); {
 
-// add logic below to verify the weather status is clear
+}
 
-// Verify shuttle launch can proceed based on above conditions
+// conditional logic to verify fuelLevel is at 100%
+if (fuelLevel === '100%');{
+
+}
+
+// conditional logic to verify weatherStatus is 'clear'
+if (weatherStatus === 'clear');{
+}
+
+// Confirm shuttle launch may safely proceed per above conditions
+
+if (astronautCount <= 7) {  // verify astronautCount does not exceed 7
+    if (astronautStatus === 'ready') {  // verify astronautStatus is 'ready'
+        console.log(`date: ${date}`);
+        console.log(`time: ${time}`);
+        console.log(`astronaut Count: ${astronautCount}`);
+        console.log(`Crew Mass: ${crewMassKg} kg`);
+    }
+    if (fuelLevel === '100%') {  // verify fuelLevel is at 100%
+        console.log(`Fuel Mass: ${fuelMassKg} kg`);
+        console.log(`Shuttle Mass: ${shuttleMassKg} kg`);
+        console.log(`Total Mass: ${totalMassKg}`);
+        if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {  // verify fuel temperature in the required range
+            console.log(`Fuel Temperature: ${fuelTempCelsius} \u00B0C`);
+        }
+        if (weatherStatus === 'clear') {  // verify weatherStatus is 'clear'
+            console.log(`Weather Status: ${weatherStatus}`);
+            console.log(`\nEveryone here at Mission Control sends the ${astronautCount} of you best wishes for a safe and successful mission`);
+        }
+    }
+} else {
+    // not date and time of aborted mission / clock stop
+    console.log(`date: ${date}`);
+    console.log(`time: ${time}`);
+    console.log('Terminate count down.  \nConditions for safe mission have not been achieved!');
+}
+
+/*
+JavaScript template literals, add 'unicode characters' like degree symbol-
+use escape sequence \u  symbol/character's unicode code point in hexadecimal.
+https://stackoverflow.com/questions/10104634/how-to-convert-00b0-degree-sign-unicode-character
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+https://stackoverflow.com/questions/66958602/how-to-write-special-characters-in-template-literals
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Character_escape
+
+*/
