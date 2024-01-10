fa8ac77008f8462b54ebd8f37b458b789e5f52d3
Billie-LS <lskylizard@yahoo.com>
2024-01-10 14:25:22 -0600

    variables initialize

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,6 +1,22 @@
-// Initialize Variables below
+// variable initialization
+let date = ('Monday 2019-03-18');
+let time = ('10:05:34 AM');
+let astronautCount = parseFloat(7);
+let astronautStatus = ('ready');
+let averageAstronautMassKg = parseFloat(80.7);
+let crewMassKg = parseFloat(astronautCount * averageAstronautMassKg);
+let fuelMassKg = parseFloat(760000);
+let shuttleMassKg = parseFloat(74842.31);  // shuttle mass in Kg
+let totalMassKg = parseFloat(crewMassKg + fuelMassKg + shuttleMassKg); // total mass in Kg
+let maximumMassLimit = parseFloat(850000); // maximum mass limit in Kg
+let fuelTempCelsius = Number(-225);
+let maximumFuelTemp = Number(-150);
+let fuelLevel = ('100%');
+let weatherStatus = ('clear');
+let preparedForLiftOff = true; // boolean indicating 'go' or 'no-go'
 
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+
+// conditional logic to verify astronautCount for shuttle does not exceed 7
 
 // add logic below to verify all astronauts are ready
 
