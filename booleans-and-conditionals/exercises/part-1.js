efac0831eab77556459a08562d7ce0d0e7995a7a
Billie-LS <lskylizard@yahoo.com>
2024-01-09 19:01:00 -0600

    part-1

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -16,3 +16,42 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+// crewStatus = (spaceSuitsOn && shuttleCabinReady)
+if (crewStatus) {  // so, is crewStatus true + true = true
+   console.log('Crew Ready');
+} else{
+   console.log('Crew Not Ready');
+}
+
+
+// computerStatusCode = 200
+if (computerStatusCode === 200) {  // true
+   console.log('Please stand by. Computer is rebooting.');
+} else if (computerStatusCode === 400){
+   console.log('Success! Computer online.');
+} else {
+   console.log('ALERT: Computer offline!');
+}
+
+
+// shuttleSpeed = 15000
+if (shuttleSpeed > 17500) {  // false
+   console.log('ALERT: Escape velocity reached!');
+} else if (shuttleSpeed < 8000){  // false
+   console.log('ALERT: Cannot maintain orbit!');
+} else {
+   console.log('Stable speed');
+}
+
+if (crewStatus && (computerStatusCode === 200) && spaceSuitsOn) {
+   console.log("all systems go");
+} else {
+   console.log("WARNING. Not ready");
+}
+
+if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+   console.log("WARNING. Not ready");
+} else {
+   console.log("all systems go");
+}
