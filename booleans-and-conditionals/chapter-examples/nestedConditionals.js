9f66764e7bacbdb3a4ef7365a8a6d55ed29ecc2f
Billie-LS <lskylizard@yahoo.com>
2024-01-09 18:20:52 -0600

    conditionals and booleans

---
+++ booleans-and-conditionals/chapter-examples/nestedConditionals.js
@@ -0,0 +1,34 @@
+// let num = 7;
+
+// if (num % 2 === 0) {
+//    console.log("EVEN");
+// }
+
+// if (num > 0) {
+//    console.log("POSITIVE");
+// }
+
+let num1 = 7;
+
+if (num1 % 2 === 0) {
+    console.log("EVEN");
+
+    if (num1 > 0) {
+        console.log("POSITIVE");
+    }
+}
+
+let num = 7;
+
+if (num % 2 === 0) {
+    if (num % 2 === 1) {
+        console.log("odd");
+    }
+}
+
+let num3 = 3;
+if (num3 % 2 === 0) {
+    if (num3 % 2 === 1) {
+        console.log("odd");
+    }
+}
