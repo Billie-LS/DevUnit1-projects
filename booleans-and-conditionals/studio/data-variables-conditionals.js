ba85700d2e94d1698688f7b995b00c2a58e78480
Billie-LS <lskylizard@yahoo.com>
2024-01-12 16:01:42 -0600

    studio 1 work conditionals and booleans

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -63,44 +63,126 @@ Conditional (ternary operator)
 
 // Confirm shuttle launch may safely proceed per above conditions
 
-if (astronautCount <= 7) {  // verify astronautCount does not exceed 7
+if (astronautCount <= 7) {  // Verify astronautCount does not exceed 7
     if (astronautStatus === 'ready') {  // verify astronautStatus is 'ready'
         console.log(`date: ${date}`);
         console.log(`time: ${time}`);
         console.log(`astronaut Count: ${astronautCount}`);
         console.log(`Crew Mass: ${crewMassKg} kg`);
-    }
-    if (fuelLevel === '100%') {  // verify fuelLevel is at 100%
-        console.log(`Fuel Mass: ${fuelMassKg} kg`);
-        console.log(`Shuttle Mass: ${shuttleMassKg} kg`);
-        console.log(`Total Mass: ${totalMassKg}`);
-        if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {  // verify fuel temperature in the required range
-            console.log(`Fuel Temperature: ${fuelTempCelsius} \u00B0C`);
-        }
-        if (weatherStatus === 'clear') {  // verify weatherStatus is 'clear'
-            console.log(`Weather Status: ${weatherStatus}`);
-            console.log(`\nEveryone here at Mission Control sends the ${astronautCount} of you best wishes for a safe and successful mission`);
+        if (fuelLevel === '100%') {  // Verify fuelLevel is at 100%
+            console.log(`Fuel Mass: ${fuelMassKg} kg`);
+            console.log(`Shuttle Mass: ${shuttleMassKg} kg`);
+            console.log(`Total Mass: ${totalMassKg}`);
+            if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {  // Verify fuel temperature in the required range
+                // use escape sequence \u  symbol/character's unicode code point in hexadecimal degree celsius
+                console.log(`Fuel Temperature: ${fuelTempCelsius} \u00B0C`);
+                if (weatherStatus === 'clear') {  // Verify weatherStatus is 'clear'
+                    console.log(`Weather Status: ${weatherStatus}`);
+                    console.log(`\nEveryone here at Mission Control sends the ${astronautCount} of you best wishes for a safe and successful mission`);
+                    preparedForLiftOff = true;
+                }
+            }
         }
     }
 } else {
-    // not date and time of aborted mission / clock stop
+    // note date and time of aborted mission / clock stop
     console.log(`date: ${date}`);
     console.log(`time: ${time}`);
     console.log('Terminate count down.  \nConditions for safe mission have not been achieved!');
+    preparedForLiftOff = false;
 }
 
 
 
+// Verify shuttle may proceed with launch 
+if (astronautCount <= 7) {  // Verify astronautCount does not exceed 7
+    if (astronautStatus === 'ready') {  // Verify astronautStatus is 'ready'
+        if (fuelLevel === '100%') {  // Verify fuelLevel is at 100%
+            if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {  // Verify fuel temperature in the required range
+                if (weatherStatus === 'clear') {  // Verify weatherStatus is 'clear'
+                    preparedForLiftOff = true;
+                }
+            }
+        }
+    }
+} else {
+    // note date and time of aborted mission / clock stop
+    preparedForLiftOff = false;
+}
+
+if (preparedForLiftOff === true) {
+    console.log('----------------------------------------------------------------');
+    console.log("All systems green. Initiating shuttle launch sequence.");
+    console.log('----------------------------------------------------------------');
+    console.log(`Date: ${date}`);
+    console.log(`Time: ${time}`);
+    console.log(`Crew Count: ${astronautCount}`);
+    console.log(`Crew Mass: ${crewMassKg}`);
+    console.log(`Fuel Mass: ${fuelMassKg}`);
+    console.log(`Shuttle Mass: ${shuttleMassKg}`);
+    console.log(`Total Mass: ${totalMassKg}`);
+    console.log(`Fuel Temperature: ${fuelTempCelsius} \u00B0C`);
+    console.log(`Weather Status: ${weatherStatus}`);
+    console.log(`\nEveryone here at Mission Control sends the ${astronautCount} of you best wishes for a safe and successful mission`);
+} else {
+    console.log('----------------------------------------------------------------');
+    console.log("Systems check failure.  Condition Red.");
+    console.log('----------------------------------------------------------------');
+    console.log(`Date: ${date}`);
+    console.log(`Time: ${time}`);
+    console.log('Terminate count down.  \nConditions for safe mission have not been achieved!');
+}
 
 
+// Verify shuttle may proceed with launch 
+if (astronautCount <= 7) {  // Verify astronautCount does not exceed 7
+    if (astronautStatus === 'ready') {  // Verify astronautStatus is 'ready'
+        if (fuelLevel === '100%') {  // Verify fuelLevel is at 100%
+            if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {  // Verify fuel temperature in the required range
+                if (weatherStatus === 'clear') {  // Verify weatherStatus is 'clear'
+                    preparedForLiftOff = true;
+                }
+            }
+        }
+    }
+} else {
+    // note date and time of aborted mission / clock stop
+    preparedForLiftOff = false;
+}
 
+// Ternary operator used
+preparedForLiftOff = (
+    astronautCount <= 7 && 
+    astronautStatus === 'ready' &&
+    fuelLevel === '100%' &&
+    fuelTempCelsius >= -300 &&
+    fuelTempCelsius <= -150 &&
+    weatherStatus === 'clear') 
+    ? true : false;
 
-
-
-
-
-
-
+(preparedForLiftOff === true) ? 
+(
+console.log('----------------------------------------------------------------'),
+console.log("All systems green. Initiating shuttle launch sequence."),
+console.log('----------------------------------------------------------------'),
+console.log(`Date: ${date}`),
+console.log(`Time: ${time}`),
+console.log(`Crew Count: ${astronautCount}`),
+console.log(`Crew Mass: ${crewMassKg}`),
+console.log(`Fuel Mass: ${fuelMassKg}`),
+console.log(`Shuttle Mass: ${shuttleMassKg}`),
+console.log(`Total Mass: ${totalMassKg}`),
+console.log(`Fuel Temperature: ${fuelTempCelsius} \u00B0C`),
+console.log(`Weather Status: ${weatherStatus}`),
+console.log(`\nEveryone here at Mission Control sends the ${astronautCount} of you best wishes for a safe and successful mission`)
+)
+: (console.log('----------------------------------------------------------------'),
+console.log("Systems check failure.  Condition Red."),
+console.log('----------------------------------------------------------------'),
+console.log(`Date: ${date}`),
+console.log(`Time: ${time}`),
+console.log('Terminate count down.  \nConditions for safe mission have not been achieved!')
+)
 
 
 
@@ -114,3 +196,32 @@ https://stackoverflow.com/questions/66958602/how-to-write-special-characters-in-
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Character_escape
 
 */
+
+
+/*
+if (astronautCount <= 7) {  // verify astronautCount does not exceed 7
+    if (astronautStatus === 'ready') {  // verify astronautStatus is 'ready'
+        console.log(`date: ${date}`);
+        console.log(`time: ${time}`);
+        console.log(`astronaut Count: ${astronautCount}`);
+        console.log(`Crew Mass: ${crewMassKg} kg`);
+    }
+    if (fuelLevel === '100%') {  // verify fuelLevel is at 100%
+        console.log(`Fuel Mass: ${fuelMassKg} kg`);
+        console.log(`Shuttle Mass: ${shuttleMassKg} kg`);
+        console.log(`Total Mass: ${totalMassKg}`);
+        if (fuelTempCelsius >= -300 && fuelTempCelsius <= -150) {  // verify fuel temperature in the required range
+            console.log(`Fuel Temperature: ${fuelTempCelsius} \u00B0C`);  // use escape sequence \u  symbol/character's unicode code point in hexadecimal
+        }
+        if (weatherStatus === 'clear') {  // verify weatherStatus is 'clear'
+            console.log(`Weather Status: ${weatherStatus}`);
+            console.log(`\nEveryone here at Mission Control sends the ${astronautCount} of you best wishes for a safe and successful mission`);
+        }
+    }
+} else {
+    // note date and time of aborted mission / clock stop
+    console.log(`date: ${date}`);
+    console.log(`time: ${time}`);
+    console.log('Terminate count down.  \nConditions for safe mission have not been achieved!');
+}
+ */
