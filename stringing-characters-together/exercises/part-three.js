b1b7fd16716b271c7b62a75ce237f31f973e614b
Billie-LS <lskylizard@yahoo.com>
2024-01-17 22:13:43 -0600

    stringing exercise

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -31,3 +31,10 @@ language.replace('Java', language.charAt(0)).replace('Script', language.charAt(4
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+let titleCase = notTitleCase.replace('t', 'T').replace('c', 'C');
+console.log(titleCase);
+
+console.log(notTitleCase);
+let titleCaseA = notTitleCase.replace('t','T') + notTitleCase.replace('c','C');
+console.log(titleCaseA);
+
