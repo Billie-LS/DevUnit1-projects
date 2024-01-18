2dcbf110b472cae3f0d1cf5e1586314775d0a67f
Billie-LS <lskylizard@yahoo.com>
2024-01-17 21:21:18 -0600

    stringing exercise

--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,12 +4,20 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 console.log(language.slice(0,1)+language.slice(4,5));
+console.log(language.slice(0,1).concat(language.slice(4,5)));
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
-console.log(language.replace('JavaScript','JS'));
+console.log(`one ${language.replace('JavaScript','JS')}`);
+
+console.log(`two ${language1a = language.charAt(0) + language.charAt(4)}`);
+console.log(`three ${language1b = language.charAt(0).concat(language.charAt(4))}`);
+
+console.log(`four ${language1c = language.substring(0, 1) + language.substring(4, 5)}`);
+console.log(`five ${language1d = language.substring(0, 1).concat(language.substring(4, 5))}`);
 
 let initials = language.replace('Java', language.charAt(0)).replace('Script', language.charAt(4)); 
 console.log(initials);
+
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 console.log(`The abbreviation for ${language} is ${initials}.`);
 
