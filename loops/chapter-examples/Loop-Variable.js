04090261fe29d3c016b976c0669674e789d2c414
Billie-LS <lskylizard@yahoo.com>
2024-01-19 12:31:44 -0600

    basic loop iteration examples

--- loops/chapter-examples/Loop-Variable.js
+++ loops/chapter-examples/Loop-Variable.js
@@ -1,5 +1,21 @@
-// Experiment with this loop by modifying each of the following: the variable initialization, the boolean condition, and the update expression.
+/*
+Experiment with this loop by modifying each of the following: 
+the variable initialization, 
+the boolean condition, 
+and the update expression.
+*/
 
+// Declare a variable 'i' and initialize it to 0.
+// The loop will start from the value assigned to 'i'.
 for (let i = 0; i < 51; i++) {
-   console.log(i);
+   console.log(i);  // Log the current value of 'i' to the console.
+   // Increment the value of 'i' by 1 in each iteration.
+   // The loop will continue as long as 'i' is less than 51.
  }
+
+// Output:
+// 0
+// 1
+// 2
+// ...
+// 50
