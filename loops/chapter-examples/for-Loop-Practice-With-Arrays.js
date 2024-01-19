fae4bd4419926d30aed6c21306757a100b4dfddf
Billie-LS <lskylizard@yahoo.com>
2024-01-19 13:43:31 -0600

    examples, for loop with arrays

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,10 @@
 // create an array variable containing the names
-
+let namesFamily = [
+    'Taco',
+    'Panagale',
+    'billie'
+]
 // write a for loop that prints each name on a different line
+for (let i = 0; i < namesFamily.length; i++){
+    console.log(namesFamily[i]);
+}
