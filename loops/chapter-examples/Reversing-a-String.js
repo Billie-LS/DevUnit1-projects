4ab628dfbe0bff40bd9319be882a7e5900f6a6c4
Billie-LS <lskylizard@yahoo.com>
2024-01-20 14:50:27 -0600

    for loop reversing string/s

--- loops/chapter-examples/Reversing-a-String.js
+++ loops/chapter-examples/Reversing-a-String.js
@@ -1,11 +1,21 @@
 let str = "blue";  // declare variable str and initialize to value "blue"
-let reversed = "";
+let reversed = "";  // accumulator variable
 
 // Loops through each character string 'blue'
 for (let i = 0; i < str.length; i++) {
     // concatenate current character to the beginning of the reversed string
     reversed = str[i] + reversed
-    // reversed += str[i];  // reversed = str[i] + reversed
+    // reversed += str[i];  // will NOT reverse: reversed = reversed + str[i]
 }
 
 console.log(reversed);
+
+// Summing an Array
+
+let numbers = [2, -5, 13, 42];
+let total = 0;  // accumulator variable
+
+for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];  // total = total + numbers[i]
+}
+console.log(total);
