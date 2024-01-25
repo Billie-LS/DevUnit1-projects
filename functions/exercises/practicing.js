33067213c94fd47c54eb9ed1068b40f838954ada
Billie-LS <lskylizard@yahoo.com>
2024-01-25 17:28:11 -0600

    functions

---
+++ functions/exercises/practicing.js
@@ -0,0 +1,26 @@
+function plusTwo(num) {
+    return num + 2;
+}
+
+let a = 2;
+
+for (let i=0; i < 4; i++) {
+    a = plusTwo(a);
+}
+
+console.log(a);
+
+function repeater(str) {
+    let repeated = str + str;
+    // console.log(repeated);
+}
+
+console.log(repeater('Bob'));
+
+let num = 42;
+
+function isEven (num) {
+    return num % 2 === 0;
+}
+
+console.log(isEven(43));
