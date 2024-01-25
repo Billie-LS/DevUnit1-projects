6ca1b99f588c161e9067b3259f67995b2b3a28d9
Billie-LS <lskylizard@yahoo.com>
2024-01-24 18:33:53 -0600

    still recursive

--- functions/try-it/printMessage.js
+++ functions/try-it/printMessage.js
@@ -8,3 +8,30 @@ printMessage();
 message = "Goodbye";
 printMessage();
 
+function plusTwo(num) {
+   return num + 2;
+}
+
+let a = 2;
+
+for (let i=0; i < 4; i++) {
+   a = plusTwo(a);
+   // 4, 6, 8, 10
+}
+
+console.log(a);
+
+function repeater(str) {
+   let repeated = str + str;
+   console.log(repeated);
+}
+
+repeater('Bob');
+// BobBob
+
+function repeater(str) {
+   let repeated = str + str;
+   console.log(repeated);
+}
+
+repeater('Bob');
