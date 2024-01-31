902da278fd5c6eea5de085b9b1ad9ebeda14d236
Billie-LS <lskylizard@yahoo.com>
2024-01-30 20:51:56 -0600

    arrays like-strings

--- arrays/examples/like-strings.js
+++ arrays/examples/like-strings.js
@@ -1,6 +1,26 @@
+let emptyArray = [];
+console.log(emptyArray.length);
+
+let programmingLanguages = ["JavaScript", "Python", "Java", "C#"];
+console.log(programmingLanguages.length);
+
+// different format 
+let javaScriptFrameworks = [
+    "React",
+    "Angular",
+    "Ember",
+    "Vue"
+];
+
+// different data types of elements in arrays
+let grabBag = ["A string value", true, 99, 105.5];
+
+
+
 // Check Your Understanding
-let classes = ["science, computer, art"];  // array length of 1
+let classes = ['science, computer, art'];  // array length of 1
 console.log(classes.length);
+
 let teachers = ["Jones", "Willoughby", "Rhodes"];  // array length of 3
 console.log(teachers.length);
 
@@ -14,6 +34,6 @@ classes1 = classes1[0].split(',');
 console.log(classes1); // Output: [ 'science', ' computer', ' art' ]
 
 let classes2 = ["science, computer, art"];
-classes2 = classes2[0].split(',').map(item => item.trim());
-console.log(classes2); // Output: [ 'science', ' computer', ' art' ]
+classes2 = classes2[0].split(',').map(element => element.trim());
+console.log(classes2); // Output: [ 'science', 'computer', 'art' ]
 
