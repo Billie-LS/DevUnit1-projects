dbdf8d4c20e6259351c1633c6a29cde941e7e23c
Billie-LS <lskylizard@yahoo.com>
2024-02-01 09:22:32 -0600

    ObjectExercises

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,8 +12,35 @@ let salamander = {
    age: 5
 };
 
+// Create More Objects
+// Based on the two object literals provided above, create new object literals for three more animals:
+
+let superChimpTwo = {
+   name: 'Brad',
+   species: 'Chimpanzee',
+   mass: 11,
+   age: 6
+};
+
+let dog = {
+   name: 'Leroy',
+   species: 'Beagle',
+   mass: 14,
+   age: 5
+};
+
+let segmentedMicroAnimal = {
+   name: 'Almina',
+   species: 'Tardigrade',
+   mass: 0.0000000001,
+   age: 1
+};
 
 // After you have created the other object literals, add the astronautID property to each one.
+function AddAstroID(array){
+   
+}
+
 
 // Create an array to hold the animal objects.
 
