9e54865da164f2d9e6cc9bf1004e033d3498313b
Billie-LS <lskylizard@yahoo.com>
2024-02-01 22:28:57 -0600

    studio/ObjectsStudio01.js

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,7 +1,45 @@
-// Code your selectRandomEntry function here:
+// TODO: Code your selectRandomEntry function here:
 
 
-// Code your buildCrewArray function here:
+// let idNumbers = [291, 414, 503, 599, 796, 890];
+
+// function selectRandomEntry takes candidates as parameter
+function selectRandomEntry(candidates) {
+  // Get random index between 0 and < idNumbers array length
+  const randomIndex = Math.floor(Math.random() * idNumbers.length);
+
+  // Get random ID number from idNumbers array using random index
+  const randomIdNumber = idNumbers[randomIndex];
+
+  // Find candidate in candidates array with selected ID number
+  const selectedCandidate = candidates.find(candidate => candidate.astronautID === randomIdNumber);
+
+  // Return selected candidate
+  return selectedCandidate;
+}
+
+// TODO: Code your buildCrewArray function here:
+
+// function buildCrewArray takes candidates and 
+// numEntries (number of entries/candidates to select for the crew array)
+function buildCrewArray(candidates, numEntries) {
+  // empty array for selected crew members
+  const crewArray = [];
+
+  // while loop selecting candidates until number of entries reached
+  while (crewArray.length < numEntries) {
+    // Call selectRandomEntry function to get a random candidate
+    const selectedCandidate = selectRandomEntry(candidates);
+
+    // Check selected candidate's ID is not already in crewArray
+    if (!crewArray.some(member => member.astronautID === selectedCandidate.astronautID)) {
+      // Add selected candidate to the crewArray
+      crewArray.push(selectedCandidate);
+    }
+  }
+  // Return the completed array of selected crew members
+  return crewArray;
+}
 
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
@@ -14,6 +52,7 @@ let candidateA = {
   'o2Used':function(hrs){return 0.035*hrs},
   'astronautID':414
 };
+
 let candidateB = {
   'name':'Lassie',
   'species':'dog',
@@ -21,6 +60,7 @@ let candidateB = {
   'o2Used':function(hrs){return 0.030*hrs},
   'astronautID':503
 };
+
 let candidateC = {
   'name':'Jonsey',
   'species':'cat',
@@ -28,6 +68,7 @@ let candidateC = {
   'o2Used':function(hrs){return 0.022*hrs},
   'astronautID':796
 };
+
 let candidateD = {
   'name':'Paddington',
   'species':'bear',
@@ -42,6 +83,7 @@ let candidateE = {
   'o2Used':function(hrs){return 0.010*hrs},
   'astronautID':599
 };
+
 let candidateF = {
   'name':'Hugs',
   'species':'ball python',
@@ -50,6 +92,27 @@ let candidateF = {
   'astronautID':890
 };
 
+
+
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
 
+// TODO: check
+let crew = buildCrewArray(animals, 3);
+
+// Display the selected crew members
+console.log(crew);
+
+// TODO: Use a template literal to print, '____, ____, and ____ are going to space!' 
+// Fill in the blanks with the names of the selected animals.
 // Code your template literal and console.log statements:
+
+// Print the message using a template literal
+console.log(`${crew[0].name}, ${crew[1].name}, and ${crew[2].name} are going to space!`);
+
+// destructuring to extract names of selected crew 
+const [firstAnimal, secondAnimal, thirdAnimal] = crew;
+
+// Print the message using a template literal
+console.log(`${firstAnimal.name}, ${secondAnimal.name}, and ${thirdAnimal.name} are going to space!`);
+
+
