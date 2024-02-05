fe267f9af498ea4d1c0b00782d15a93bde4f50fd
Billie-LS <lskylizard@yahoo.com>
2024-02-05 08:51:32 -0600

    exercises index.js - complete the exercise!

--- modules/exercises/index.js
+++ modules/exercises/index.js
@@ -1,3 +1,4 @@
+// TODO: complete module imports code below
 //Import modules:
 const input = require('readline-sync'); //Import readline-sync.
 const averages = require('./ScoreCalcs/averages.js') //Import functions from averages.js.
@@ -18,20 +19,20 @@ for (let i = 0; i<prompts.length; i++){
   let response = input.question(`Would you like to ${prompts[i]}? Y/N: `);
   if (response.toLowerCase()==='y'){
     if (i===0){
-      //Call 'printAll' here and pass in all necessary arguments.
+      //TODO: Call 'printAll' here and pass in all necessary arguments.
       printAll(astronauts, testTitles, scores); // args = names + tests + scores
     } else if (i===1){
       for (let j = 0; j<testTitles.length; j++){
-        let avg = averages.averageForTest(j, scores); //Call 'averageForTest' here. Pass in j and scores as arguments.
+        let avg = averages.averageForTest(j, scores); //TODO: Call 'averageForTest' here. Pass in j and scores as arguments.
         console.log(`${testTitles[j]} test average = ${avg}%.`);
       }
     } else if (i===2){
       for (let j = 0; j<astronauts.length; j++){
-        let avg = averages.averageForStudent(j, scores); //Call 'averageForStudent' here. Pass in j and scores as arguments.
+        let avg = averages.averageForStudent(j, scores); //TODO: Call 'averageForStudent' here. Pass in j and scores as arguments.
         console.log(`${astronauts[j]}'s test average = ${avg}%.`);
       }
     } else {
-      let walker = randomSelect(astronauts); //Call 'randomSelect' to pick a spacewalker from the astronauts array.
+      let walker = randomSelect(astronauts); //TODO: Call 'randomSelect' to pick a spacewalker from the astronauts array.
       console.log(`${walker} is the next spacewalker.`);
     }
   } else {
