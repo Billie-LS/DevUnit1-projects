04573c49369a7242c59201d3b6d0ecba3edd35b1
Billie-LS <lskylizard@yahoo.com>
2024-02-07 13:10:08 -0600

    unit testing chapter examples

--- unit-testing/chapter-examples/hello-jest/hello.js
+++ unit-testing/chapter-examples/hello-jest/hello.js
@@ -1,8 +1,22 @@
 function hello(name) {
-   if (name === undefined)
-       name = "World";
+    if (name === undefined)
+    name = "World";
 
-   return "Hello, " + name + "!";
+    // return "Hello, " + name ;
+    return "Hello, " + name + "!";
 }
 
 module.exports = hello;
+
+
+// check understanding
+function doStringsMatch(string1, string2){
+    if (string1 === string2) {
+        return 'Strings match!';
+    } else {
+        return 'No match!';
+    };
+};
+// Which of the following tests checks if the function properly handles case-sensitive answers.
+// expect(doStringsMatch('Flower', 'Flower')).toBe('Strings match!');
+// expect(doStringsMatch('Flower', 'flower')).toBe('No match!');
--- unit-testing/chapter-examples/hello-jest/tests/hello.test.js
+++ unit-testing/chapter-examples/hello-jest/tests/hello.test.js
@@ -1,13 +1,18 @@
-const hello = require('../hello.js');
+const hello = require('../hello.js'); // Import hello.js module
 
-describe("hello world test", function(){
+// Start test suite with description
+// Expecting the return value to be "Hello, World!"
+describe("hello", function(){ // parameters name of the function test and empty anonymous function
+// describe("hello world test", function(){
 
+      // Test hello function with specific name
       test("should return a custom message when name is specified", function(){
-         expect(hello("Jest")).toBe("Hello, Jest!");
+        expect(hello("Jest")).toBe("Hello, Jest!"); // Expected return "Hello, Jest!"
       });
 
+      // Testing the hello function without specifying a name
       it("should return a general greeting when name is not specified", function(){
-        expect(hello()).toBe("Hello, World!");
+        expect(hello()).toBe("Hello, World!"); // Expecting the return value to be "Hello, World!"
     });
 
 });
