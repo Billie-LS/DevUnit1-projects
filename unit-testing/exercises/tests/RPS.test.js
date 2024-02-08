0b89843972794b6fcff483ea3f198958453b74db
Billie-LS <lskylizard@yahoo.com>
2024-02-08 14:53:35 -0600

    unit testing TDD exercises

--- unit-testing/exercises/tests/RPS.test.js
+++ unit-testing/exercises/tests/RPS.test.js
@@ -1,12 +1,28 @@
 // import module
-const whoWon = require('../whoWon.js');
+const whoWon = require('../RPS.js');
 
-describe('whoWon', function(){
+describe("whoWon", function(){
 
     // TODO: behaves correctly when the players tie (both choose the same option). 
-    test('describe', function() {
-        let result = whoWon(); // test a number less than 5
-        expect(result).toBe(''); // Expected return '3 is less than 5.'
+    test('when player1 and player2 are same, return "TIE!" ', function() {
+        let result = whoWon('rock', 'rock'); // test identical showing
+        expect(result).toBe('TIE!'); // Expected return 'TIE!'
+    });
+
+    // TODO: behaves correctly with remainder coded combinations.
+    test('when player1 shows "rock" and player2 shows "paper", return "Player 2 wins!" ', function() {
+        let result = whoWon('rock', 'paper'); // test rock vs paper
+        expect(result).toBe('Player 2 wins!'); // Expected return 'Player 2 wins!'
+    });
+
+    test('when player1 shows "paper" and player2 shows "scissors", return "Player 2 wins!" ', function() {
+        let result = whoWon('paper', 'scissors'); // test paper vs scissors
+        expect(result).toBe('Player 2 wins!'); // Expected return 'Player 2 wins!'
+    });
+
+    test('when player1 shows "scissors" and player2 shows "rock", return "Player 2 wins!" ', function() {
+        let result = whoWon('scissors', 'rock'); // test scissors vs rock
+        expect(result).toBe('Player 2 wins!'); // Expected return 'Player 2 wins!'
     });
 
 });
