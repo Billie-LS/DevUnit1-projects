5c0b1e9b371ae12dc22a2525667c89a2fc976bc8
Billie-LS <lskylizard@yahoo.com>
2024-02-09 20:26:46 -0600

    refactoring index.js to DRY with array PASS  TDD unit testing studio

--- unit-testing/studio/index copy 3.js
+++ unit-testing/studio/index copy 3.js
@@ -1,35 +1,31 @@
-let twoThree = {
-    divisors: [2, 3], 
-    output: 'LaunchCode!' 
-};
-
-let twoFive = {
-    divisors: [2, 5], 
-    output: 'Launch Rocks! (CRASH!!!!)' 
-};
-
-let two = {
-    divisors: [2], 
-    output: 'Launch!' 
-};
-
-let three = {
-    divisors: [3], 
-    output: 'Code!' 
-};
-
-let five = {
-    divisors: [5], 
-    output: 'Rocks!' 
-};
-
 let launchcode = {
     organization: "nonprofit",
     executiveDirector: "Jeff",
     percentageCoolEmployees: 100,
     programsOffered: ['Web Development', 'Data Analysis', 'Liftoff'],
     launchOutput: function (num) {
-        const conditions = [twoThree, twoFive, two, three, five];
+        const conditions = [
+            {
+                divisors: [2, 3], 
+                output: 'LaunchCode!' 
+            },
+            {
+                divisors: [2, 5], 
+                output: 'Launch Rocks! (CRASH!!!!)' 
+            },
+            {
+                divisors: [2], 
+                output: 'Launch!' 
+            },
+            {
+                divisors: [3], 
+                output: 'Code!' 
+            },
+            {
+                divisors: [5], 
+                output: 'Rocks!' 
+            },
+        ];
     
         let output = ''; // empty string to store output
     
@@ -48,6 +44,9 @@ let launchcode = {
         return 'Bang!';
     }
 };
+
+
+
 
 // Export the launchcode object for external use
 module.exports = {launchcode};
