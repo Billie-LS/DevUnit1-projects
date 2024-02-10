b583c84ad60174954e89dfe95cf2edf345895a82
Billie-LS <lskylizard@yahoo.com>
2024-02-09 20:15:44 -0600

    refactoring index.js to DRY with array PASS  TDD unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -5,14 +5,24 @@ let launchcode = {
     programsOffered: ['Web Development', 'Data Analysis', 'Liftoff'],
     launchOutput: function (num) {
         const conditions = [
-            { divisors: [2, 3], output: 'LaunchCode!' },
-            { divisors: [2, 5], output: 'Launch Rocks! (CRASH!!!!)' },
-            { divisors: [2], output: 'Launch!' },
-            { divisors: [3], output: 'Code!' },
-            { divisors: [5], output: 'Rocks!' },
+            { divisors: [2, 3], 
+                output: 'LaunchCode!' 
+            },
+            { divisors: [2, 5], 
+                output: 'Launch Rocks! (CRASH!!!!)' 
+            },
+            { divisors: [2], 
+                output: 'Launch!' 
+            },
+            { divisors: [3], 
+                output: 'Code!' 
+            },
+            { divisors: [5], 
+                output: 'Rocks!' 
+            },
         ];
     
-        let output = ''; // Initialize an empty string to store the output
+        let output = ''; // empty string to store output
     
         // Iterate over each condition in the 'conditions' array
         for (let condition of conditions) {
