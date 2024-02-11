f08f6a23ce7dec61f6d658126ff8644b0200c815
Billie-LS <lskylizard@yahoo.com>
2024-02-10 19:28:15 -0600

    throw exceptions

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,7 +1,26 @@
-// Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+/*
+Write a function called 'divide' that takes two parameters: a numerator and a denominator.
+Your function should return the result of numerator / denominator.
+However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
+Code your divide function here:
+*/
 
-// Your function should return the result of numerator / denominator.
+function divide(numerator, denominator) {
+    if (denominator === 0) {
+        throw Error('You cannot divide by zero!');
+    }
+    return numerator/denominator;
+};
 
-// However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
+const division = (numerator, denominator) => {
+    if (denominator === 0) {
+        throw new Error('You cannot divide by zero!');
+    }
+    return numerator / denominator;
+};
 
-// Code your divide function here:
+const moreDivision = (numerator, denominator) => {
+    return denominator === 0 ? 
+        (() => { throw new Error('You cannot divide by zero!'); })() :
+        numerator / denominator;
+};
--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -6,6 +6,21 @@ function gradeLabs(labs) {
   }
 }
 
+function gradeLabs(labs) {
+  for (let i = 0; i < labs.length; i++) {
+    let lab = labs[i];
+    let result;
+    try {
+      result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    } catch (error) {
+      result = "Error thrown";
+      console.log(`${lab.student} code encountered an error: ${error.message}`);
+    }
+  }
+}
+
+
 let studentLabs = [
   {
     student: 'Carly',
@@ -22,3 +37,26 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
