cb5b33beb5ebe9160f96ea25c78217f76a8a8567
Billie-LS <lskylizard@yahoo.com>
2024-02-10 18:21:02 -0600

    scope

--- scope/chapter-examples/variable-shadowing.js
+++ scope/chapter-examples/variable-shadowing.js
@@ -16,3 +16,15 @@ let name = input.question("Please enter your name: ");
 hello(name);
 doubleName(name);
 console.log(name);
+
+console.log('testing');
+
+let a = 0;
+
+function myFunction() {
+    let a = 10;
+    console.log(a);
+    return a;
+}
+myFunction()
+console.log(a);
