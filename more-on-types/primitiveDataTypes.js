51d39faf0232dc267795f5155baa80ff9ede3100
Billie-LS <lskylizard@yahoo.com>
2024-02-10 18:37:02 -0600

    more on data types

---
+++ more-on-types/primitiveDataTypes.js
@@ -0,0 +1,13 @@
+// undefined is a primitive data type in JavaScript
+// assigned to declared variables which have not been initialized.
+
+let x;
+console.log(x) // expected return undefined
+
+// null similar to undefined in that represents an unknown value, however, 
+// it is assigned to values that the programmer wishes to keep empty.
+
+let y = null;
+console.log(y);  // expected return null
+
+console.log(typeof x);
