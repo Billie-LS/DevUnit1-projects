8b21cbe801ae06b6207c7d864f445d67a101db92
Billie-LS <lskylizard@yahoo.com>
2024-02-11 19:19:02 -0600

    try-catch-finally

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -7,34 +7,39 @@
 // }
 
 
+// function gradeLabs takes array of lab objects as parameter
 function gradeLabs(labs) {
-  for (let i=0; i < labs.length; i++) {
+  // for loop each lab in the array
+  for (let i = 0; i < labs.length; i++) {
+
+    // retrieve lab from labs array
     let lab = labs[i];
-    let result;
+    
+    // 'try' block attempt run lab
     try {
-      result = lab.runLab(3);
+      // call runLab method of lab object with parameter 3, store the result
+      let result = lab.runLab(3);
+      
+      // output if lab's code worked based on result
       console.log(`${lab.student} code worked: ${result === 27}`);
-    } catch (e) {
+    } 
+
+    // 'catch' block to handle any errors 'e' occuring in execution
+    catch (e) {
+      // result indicates error thrown
       result = "Error thrown";
-      console.log(`${lab.student} code encountered an error: ${e.message}`);
-  }
+      
+      // output error message with type and message of error
+      console.log(`${lab.student} code encountered an error:\n type ${e.type} message ${e.message}, `);
+    } 
+    // 'finally' block run code regardless of whether an error occurred
+    finally {
+      // output message indicating this code ran
+      console.log('this actually ran\n');
+    }
   }
 }
 
-
-// function gradeLabs(labs) {
-//   for (let i = 0; i < labs.length; i++) {
-//     let lab = labs[i];
-//     let result;
-//     try {
-//       result = lab.runLab(3);
-//       console.log(`${lab.student} code worked: ${result === 27}`);
-//     } catch (error) {
-//       result = "Error thrown";
-//       console.log(`${lab.student} code encountered an error: ${error.message}`);
-//     }
-//   }
-// }
 
 
 let studentLabs = [
