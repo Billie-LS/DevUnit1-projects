ab48445afc993690ddbf91bd3d4c36b6046478c5
Billie-LS <lskylizard@yahoo.com>
2024-02-13 14:34:33 -0600

    classess examples

--- classes/chapter-examples/ClassExamples01.js
+++ classes/chapter-examples/ClassExamples01.js
@@ -1,19 +1,37 @@
+let goat = {
+  name: "Goat",
+  age: 7,
+  mass: 12,
+  catchPhrase: function (repeats) {
+    let phrase = "LaunchCode";
+    for (let i = 0; i < repeats; i++) {
+      phrase += " Rocks";
+    }
+    return phrase;
+  },
+};
+
+console.log(
+  `${goat.name} is ${goat.age} years old and has a mass of ${goat.mass} kg.`
+);
+console.log(`${goat.name} says, "${goat.catchPhrase(3)}."\n\n`);
+
 //Try adding new properties inside constructor.
 class Astronaut {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
+  constructor(name, age, mass) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+  }
 }
 
-let fox = new Astronaut('Fox', 7, 12);
+let fox = new Astronaut("Fox", 7, 12);
 
 console.log(fox);
 console.log(fox.age, fox.color);
 
 fox.age = 9;
-fox.color = 'red';
+fox.color = "red";
 
 console.log(fox);
 console.log(fox.age, fox.color);
