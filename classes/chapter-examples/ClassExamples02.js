e47510b6f1c3b722e7aa934cb8d7eed703a561a7
Billie-LS <lskylizard@yahoo.com>
2024-02-13 20:20:07 -0600

    class methods

--- classes/chapter-examples/ClassExamples02.js
+++ classes/chapter-examples/ClassExamples02.js
@@ -3,15 +3,53 @@
 // Next, set default values for 1 or more of the parameters in constructor.
 
 class Astronaut {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
+  constructor(name, age, mass) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+  }
 }
 
-let tortoise = new Astronaut('Speedy', 120);
+let tortoise = new Astronaut("Speedy", 120);
 
 console.log(tortoise.name, tortoise.age, tortoise.mass);
 
+class SpaceGoat {
+  constructor(name, age, mass = 54) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+  }
+}
+
+let beard = new SpaceGoat("Horn Boy", 120, 79, "fifty");
+
+console.log(beard.name, beard.age, beard.mass);
+
 // What happens if we call Astronaut and pass in MORE than 3 arguments? TRY IT!
+// it is ignored
+
+// check understanding
+class Car {
+  constructor(make, model, year, color, mpg) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.mpg = mpg;
+  }
+}
+let myCar = new Car("Chevy", "Astro", 1985, "gray", 20);
+console.log(typeof myCar.year); // expect - number
+
+let myCar2 = new Car("Tesla", "Model S", 2019);
+console.log(myCar2); // expect
+/*
+Car {
+  make: 'Tesla', 
+  model: 'Model S',
+  year: 2019,
+  color: undefined,
+  mpg: undefined
+}
+*/
--- classes/chapter-examples/ClassMethods.js
+++ classes/chapter-examples/ClassMethods.js
@@ -1,32 +1,50 @@
+/*
+if a method is the same for ALL objects of a class, 
+define that method outside of the constructor. 
+Each object does not need a copy of identical code. 
+Therefore, the declaration of a method outside of the constructor will not consume as much memory.
+*/
+
 // Here we assign the method inside the constructor
 class AstronautI {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-     this.reportStats = function() {
-       let stats = `${this.name} is ${this.age} years old and has a mass of ${this.mass} kg.`;
-         return stats;
-     }
-   }
- }
- 
- // Here we assign the method outside of the constructor
- class AstronautO {
-   constructor(name, age, mass){
-     this.name = name;
-     this.age = age;
-     this.mass = mass;
-   }
- 
-   reportStats() {
-     let stats = `${this.name} is ${this.age} years old and has a mass of ${this.mass} kg.`;
-     return stats;
-   }
- }
- 
- let fox = new AstronautI('Fox', 7, 12);
- let hippo = new AstronautO('Hippo', 25, 1000);
- 
- console.log(fox);
- console.log(hippo);
+  constructor(name, age, mass) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+    this.reportStats = function () {
+      let stats = `${this.name} is ${this.age} years old and has a mass of ${this.mass} kg.`;
+      return stats;
+    };
+  }
+}
+
+// Here we assign the method outside of the constructor
+class AstronautO {
+  constructor(name, age, mass) {
+    this.name = name;
+    this.age = age;
+    this.mass = mass;
+  }
+
+  reportStats() {
+    let stats = `${this.name} is ${this.age} years old and has a mass of ${this.mass} kg.`;
+    return stats;
+  }
+}
+
+let fox = new AstronautI("Fox", 7, 12);
+let hippo = new AstronautO("Hippo", 25, 1000);
+
+console.log(fox);
+console.log(hippo);
+
+class Plant {
+  constructor(type, height) {
+    this.type = type;
+    this.height = height;
+  }
+
+  grow() {
+    this.height = this.height + 1;
+  }
+}
