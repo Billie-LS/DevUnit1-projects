7ea2e8f0861c75c733147a378349e2c091a65cf3
Billie-LS <lskylizard@yahoo.com>
2024-02-15 14:28:46 -0600

    check concepts

---
+++ classes/exercises/Read and Concept Check.js
@@ -0,0 +1,26 @@
+class Car {
+  constructor(make, model, year, color, mpg) {
+    (this.make = make),
+      (this.model = model),
+      (this.year = year),
+      (this.color = color),
+      (this.mpg = mpg);
+  }
+}
+
+let myCar = new Car("Chevy", "Astro", 1985, "gray", 20);
+console.log(myCar);
+console.log(typeof myCar.year);
+
+let myCar1 = new Car("Tesla", "Model S", 2019);
+console.log(myCar1);
+
+class Plant {
+  constructor(type, height) {
+    (this.type = type), (this.height = height);
+  }
+
+  grow() {
+    this.height = this.height + 1;
+  }
+}
