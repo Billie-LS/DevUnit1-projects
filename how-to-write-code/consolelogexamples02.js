1ca2a5fd9c204cdc6bf21a1eaaa2571b0a73f695
Billie-LS <lskylizard@yahoo.com>
2024-03-13 21:20:50 -0500

    commenting and special characters

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -5,10 +5,18 @@ console.log("Python, JavaScript, Java, C#,Swift");
 // testing \n
 console.log("\nSome Programming Languages:");
 
+console.log("\nSome Programming Languages:\n");
+
+console.log("\nSome \nProgramming \nLanguages:");
+
 console.log("Python\nJavaScript\nJava\nC#\nSwift\n");
 
 // testing \t
 console.log("\tSome Programming Languages:");
+
+console.log("\tSome Programming Languages:\t");
+
+console.log("\tSome \tProgramming \tLanguages:");
 
 console.log("\tPython\tJavaScript\tJava\tC#\tSwift");
 
