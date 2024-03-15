1aca91db47e084530f6e1a60f6a0450d7f0f3d9a
Billie-LS <lskylizard@yahoo.com>
2024-03-14 21:35:01 -0500

    typeof examples

--- data-and-variables/chapter-examples/type-of.js
+++ data-and-variables/chapter-examples/type-of.js
@@ -3,5 +3,15 @@ console.log(typeof "Hello, People");
 console.log(typeof "17");
 console.log(typeof "3.2");
 
-console.log(typeof 'This is a string');
+console.log(typeof "This is a string");
 console.log(typeof "And so is this");
+
+console.log(typeof 78);
+
+console.log(typeof 1.78);
+
+console.log(typeof [78]);
+
+console.log(typeof [78, 1, "This is a string"]);
+
+console.log(typeof console.log());
