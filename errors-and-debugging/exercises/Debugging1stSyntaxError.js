b6469d920f93a9a3d825303e6b98ea9211559772
Billie-LS <lskylizard@yahoo.com>
2024-03-29 21:47:05 -0500

    debug exercises

--- errors-and-debugging/exercises/Debugging1stSyntaxError.js
+++ errors-and-debugging/exercises/Debugging1stSyntaxError.js
@@ -4,10 +4,27 @@
 let launchReady = false;
 let fuelLevel = 17000;
 
-if (fuelLevel >= 20000){
-   console.log('Fuel level cleared.');
-   launchReady = true;
+if (fuelLevel >= 20000) {
+  console.log("Fuel level cleared.");
+  launchReady = true;
 } else {
-   console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+  console.log("WARNING: Insufficient fuel!");
+  launchReady = false;
 }
+
+let launchGood;
+
+switch (true) {
+  case fuelLevel >= 20000:
+    console.log("Fuel level cleared.");
+    launchGood = true;
+    break;
+  default:
+    console.log("WARNING: Insufficient fuel!");
+    launchGood = false;
+}
+
+let launchProper =
+  fuelLevel >= 20000
+    ? (console.log("Fuel level cleared."), true)
+    : (console.log("WARNING: Insufficient fuel!"), false);
