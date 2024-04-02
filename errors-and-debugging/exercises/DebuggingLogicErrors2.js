91e52bf26091e7d61398a874f87dbc83ac49275e
Billie-LS <lskylizard@yahoo.com>
2024-04-01 19:38:53 -0500

    debug exercises logic errors 2

--- errors-and-debugging/exercises/DebuggingLogicErrors2.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors2.js
@@ -1,5 +1,5 @@
 // Let’s break the code down into smaller chunks.
-// Consider the first if/else block below. 
+// Consider the first if/else block below.
 // Add console.log(launchReady) after this block, then run the program.
 
 //Given the fuelLevel value, should launchReady be true or false after the check?  Is the program behaving as expected?
@@ -10,15 +10,35 @@ let fuelLevel = 17000;
 // let computerStatus = 'green';
 
 if (fuelLevel >= 20000) {
-   console.log('Fuel level cleared.');
-   launchReady = true;
+  console.log("Fuel level cleared.");
+  launchReady = true;
 } else {
-   console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
-   console.log(launchReady)
+  console.log("WARNING: Insufficient fuel!");
+  launchReady = false;
+  console.log(launchReady);
 }
 
-
+// Define 'launchProper' variable
+let launchProper;
+// Switch statement to check 'fuelLevel'
+switch (true) {
+  // Case where 'fuelLevel' is greater than or equal to 20000
+  case fuelLevel >= 20000:
+    // Log message 'Fuel level cleared.' to the console
+    console.log("Fuel level cleared.");
+    // Set 'launchProper' to true
+    launchProper = true;
+    // Exit switch statement
+    break;
+  // Default case for when 'fuelLevel' is less than 20000
+  default:
+    // Log warning message to the console
+    console.log("\nWARNING: Insufficient fuel!");
+    // Set 'launchProper' to false
+    launchProper = false;
+    //  log boolean value
+    console.log(launchProper);
+}
 
 // if (crewStatus && computerStatus === 'green'){
 //    console.log('Crew & computer cleared.');
