f4a54e5018b4a123f5c0c68d4f0117c5c6ab6c7f
Billie-LS <lskylizard@yahoo.com>
2024-04-03 21:36:39 -0500

    debug exercises runtime  errors 1

--- errors-and-debugging/exercises/DebuggingRuntimeErrors1.js
+++ errors-and-debugging/exercises/DebuggingRuntimeErrors1.js
@@ -5,9 +5,33 @@ let launchReady = false;
 let fuelLevel = 17000;
 
 if (fuelLevel >= 20000) {
-   console.log('Fuel level cleared.');
-   launchReady = true;
+  console.log("Fuel level cleared.");
+  launchReady = true;
 } else {
-   console.log('WARNING: Insufficient fuel!');
-   launchReady = false;
+  console.log("WARNING: Insufficient fuel!");
+  launchReady = false;
+}
+
+// Switch statement to check 'fuelLevel'
+switch (true) {
+  // Case where 'fuelLevel' is greater than or equal to 20000
+  case fuelLevel >= 20000:
+    // Log message 'Fuel level cleared.' to the console
+    console.log("Fuel level cleared.");
+    // Set 'launchProper' to true
+    launchReady = true;
+    //  log boolean value
+    console.log(`launchReady = ${launchReady}`);
+    // Exit switch statement
+    break;
+  // Default case for when 'fuelLevel' is less than 20000
+  default:
+    // Log warning message to the console
+    console.log("\nWARNING: Insufficient fuel!");
+    // Set 'launchProper' to false
+    launchReady = false;
+    //  log boolean value
+    console.log(`launchReady = ${launchReady}`);
+    // Exit switch statement
+    break;
 }
